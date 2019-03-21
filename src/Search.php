cb8f93db4c7d96f663cac4c563fe7af92b9a4f83
Timi-Artturi Mäkelä <timi.artturi.makela@gmail.com>
2019-03-21 10:58:14 +0200

    Added  to the filter redipress/formatted_search_results

--- src/Search.php
+++ src/Search.php
@@ -204,7 +204,7 @@ class Search {
                 }
             }
 
-            $this->results = apply_filters( 'redipress/formatted_search_results', $results );
+            $this->results = apply_filters( 'redipress/formatted_search_results', $results, $raw_results );
 
             $query->found_posts = $count;
             $query->max_num_pages( ceil( $count / $query->query_vars['posts_per_page'] ) );

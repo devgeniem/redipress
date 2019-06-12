df042936aceac9dd7a9b26e11b180a6c54a7e2a2
Miika Arponen <miika@geniem.com>
2019-06-12 16:29:35 +0300

    Added update_value API function

--- src/API.php
+++ src/API.php
@@ -30,3 +30,47 @@ function get_post( $post_id ) : ?\WP_Post {
 
     return null;
 }
+
+/**
+ * Update a single value in a RediPress document
+ *
+ * @param string $doc_id Document ID to modify.
+ * @param string $field  Field to update.
+ * @param string $value  Value to update the field with.
+ * @param integer $score Possible weighing score to the value.
+ * @return array
+ */
+function update_value( $doc_id, $field, $value, $score = 1 ) {
+    $client = apply_filters( 'redipress/client', null );
+
+    $index = Admin::get( 'index' );
+
+    $result_add = $client->raw_command(
+        'FT.ADD',
+        [
+            $index,
+            $doc_id,
+            $score,
+            'REPLACE',
+            'PARTIAL',
+            'FIELDS',
+            $field,
+            $value,
+        ]
+    );
+
+    $result_add_hash = $client->raw_command(
+        'FT.ADDHASH',
+        [
+            $index,
+            $doc_id,
+            $score,
+            'REPLACE',
+        ]
+    );
+
+    return [
+        $result_add,
+        $result_add_hash,
+    ];
+}

cbf51a83e517bf1c19b2b5af211401f6528b8165
Ville Siltala <ville.siltala@geniem.com>
2020-05-11 16:52:57 +0300

    Fix main query localization when querying multiple sites

--- src/External/Polylang.php
+++ src/External/Polylang.php
@@ -14,8 +14,74 @@ class Polylang {
      * Constructor
      */
     public function __construct() {
+        // Filter the query before RediPress to handle localizations.
+        add_filter( 'posts_pre_query', [ $this, 'posts_pre_query' ], 9, 2 );
         // Add the query var filter
         add_filter( 'redipress/query_vars', [ $this, 'add_query_var' ], 10, 1 );
+    }
+
+    /**
+     * Filter WordPress posts requests before RediPress.
+     *
+     * This fixes the main query on multisites where the main query
+     * is set to query posts from multiple sites.
+     *
+     * @param array|null $posts An empty array of posts.
+     * @param \WP_Query  $query The WP_Query object.
+     * @return array Results or null if no results.
+     */
+    public function posts_pre_query( ?array $posts, \WP_Query $query ) : ?array {
+        if ( method_exists( $query, 'is_main_query' ) && $query->is_main_query() ) {
+            $blog_id  = \get_current_blog_id();
+            $blog_var = $query->get( 'blog' );
+
+            // If querying from all blogs or
+            // the blog array contains more than just the current blog id or
+            // the blog id doesn't match the current one.
+            if (
+                $blog_var === 'all' ||
+                (
+                    is_array( $blog_var ) &&
+                    count( [ $blog_id ] + $blog_var ) > 1
+                ) ||
+                (
+                    is_scalar( $blog_var ) &&
+                    intval( $blog_var ) !== $blog_id
+                )
+            ) {
+                $tax_queries = $query->get( 'tax_query' ) ?? [];
+
+                if ( empty( $tax_queries ) ) {
+                    return $posts;
+                }
+
+                // Find the PLL language query and replace the id with the slug.
+                foreach ( $tax_queries as $idx => $tax_query ) {
+                    $taxonomy = $tax_query['taxonomy'] ?? '';
+                    $field    = $tax_query['field'] ?? '';
+
+                    if ( $taxonomy === 'language' && $field === 'term_taxonomy_id' ) {
+                        $term      = \get_term( $tax_query['terms'] );
+
+                        // Convert the id query to a slug query.
+                        $slug_query = [
+                            'taxonomy' => 'language',
+                            'field'    => 'slug', // Since WP 3.5
+                            'terms'    => [ $term->slug ?? '' ],
+                            'operator' => 'IN',
+                        ];
+
+                        // Replace the query.
+                        $tax_queries[ $idx ] = $slug_query;
+                    }
+                }
+
+                // Recreate the tax queries.
+                $query->tax_query = new \WP_Tax_Query( $tax_queries );
+            }
+        }
+
+        return $posts;
     }
 
     /**

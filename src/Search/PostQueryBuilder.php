696d4ef6ad5f8361732a24032b7aaecde60e5541
Timi-Artturi Mäkelä <timi.makela@geniem.com>
2022-08-02 09:57:53 +0300

    clause hotfix

--- src/Search/PostQueryBuilder.php
+++ src/Search/PostQueryBuilder.php
@@ -889,6 +889,7 @@ class PostQueryBuilder extends QueryBuilder {
 
                     // If we have a distance clause, just pass it on
                     if (
+                        ! empty( $clause['order'] ) &&
                         is_array( $clause['order']['compare'] ) &&
                         ! empty( $clause['order']['compare']['lat'] ) &&
                         ! empty( $clause['order']['compare']['lng'] )

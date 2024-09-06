c9919b6e7012bf902f5dd7fa89d33311823c81a1
Ville Löytynoja <ville.loytynoja@geniem.com>
2024-09-06 08:03:05 +0300

    CR fixes.

--- src/Search/QueryBuilder.php
+++ src/Search/QueryBuilder.php
@@ -539,9 +539,9 @@ abstract class QueryBuilder {
     /**
      * WP_Query meta_query parameter.
      *
-     * @return string|null
+     * @return string
      */
-    protected function meta_query() : ?string {
+    protected function meta_query() : string {
         if ( ! empty( $this->meta_query ) ) {
             return $this->meta_query;
         }
